8ce9bba332545c1f0d729839c601cf34c4398b0e
beefysalad <jpatrickzephyr@gmail.com>
2026-03-04 02:25:40 +0800

    feat: initial clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { QueryProvider } from './providers/query-provider'
 import { ThemeProvider } from './providers/theme-provider'
-
+import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'sonner'
 import { SessionProvider } from 'next-auth/react'
 import { Analytics } from '@vercel/analytics/next'
@@ -30,25 +30,27 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <SessionProvider>
-            <QueryProvider>
-              {children}
-              <Toaster richColors position="bottom-center" />
-            </QueryProvider>
-            <Analytics />
-          </SessionProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <SessionProvider>
+              <QueryProvider>
+                {children}
+                <Toaster richColors position="bottom-center" />
+              </QueryProvider>
+              <Analytics />
+            </SessionProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
